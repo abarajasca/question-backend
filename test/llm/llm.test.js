3d52597b2bebab440740c3cf29734f75fc464e33
Adrian Barajas <a_barajas_c@hotmail.com>
2024-11-19 21:11:53 -0600

    Update template and clean import in test

--- test/llm/llm.test.js
+++ test/llm/llm.test.js
@@ -1,6 +1,6 @@
 import * as chai from 'chai'
 import { default as chaiHttp } from "chai-http";
-import { after, describe } from 'mocha';
+import { describe } from 'mocha';
 import { expect } from 'chai'
 import { request } from 'chai-http';
 
